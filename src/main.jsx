302abf255b15d504f9e7dc4a536a589f83cc8275
Jose Zavala <jazavala@umsa.bo>
2025-06-25 13:32:53 -0400

    Fix card.jsx import error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar.jsx';
-import Card from './card.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import Footer from './Footer.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
